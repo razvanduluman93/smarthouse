d18dc43761bf4ced235e8aeab0ea2fcb894f6a02
stefan cristian <Cristian.Stefan@cegeka.com>
2019-10-28 18:53:35 +0200

    Removed the credentials from the code.

--- src/main/java/edu/nocturne/java/smarthouse/SmarthouseApplication.java
+++ src/main/java/edu/nocturne/java/smarthouse/SmarthouseApplication.java
@@ -6,11 +6,15 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.*;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,10 +27,6 @@ import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 @SpringBootApplication
 public class SmarthouseApplication {
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
     @Value("${cloud.aws.dynamodb.tables.DeviceEvents.name}")
@@ -39,9 +39,41 @@ public class SmarthouseApplication {
     }
 
     @Bean
+    public BasicAWSCredentials basicAWSCredentials() {
+        String accessKey;
+        String secretKey;
+        String secretName = "AWS";
+        String region = "eu-west-1";
+
+        AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard()
+                                                                 .withRegion(region)
+                                                                 .build();
+
+        String secret = null;
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+                .withSecretId(secretName);
+        GetSecretValueResult getSecretValueResult;
+
+        try {
+            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+        } catch (DecryptionFailureException | InternalServiceErrorException | InvalidParameterException | InvalidRequestException | ResourceNotFoundException e) {
+            throw e;
+        }
+
+        if (getSecretValueResult.getSecretString() != null) {
+            secret = getSecretValueResult.getSecretString();
+        }
+
+        // Your code goes here.
+        JSONObject jsonObject = new JSONObject(secret);
+        accessKey = jsonObject.getString("accessKey");
+        secretKey = jsonObject.getString("secretKey");
+        return new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    @Bean
     public DynamoDB dynamoDB() {
-        AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,
-                                                                                                                    secretKey));
+        AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(basicAWSCredentials());
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                                                            .withRegion(region)
                                                            .withCredentials(credentialsProvider)
